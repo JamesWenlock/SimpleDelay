f07c979df6b6373e4021172ed74ece559f2f058b
JamesWenlock <wenloj@uw.edu>
2018-08-07 21:36:44 -0700

    initial push

---
+++ SimpleDelay/Source/PluginEditor.cpp
@@ -0,0 +1,59 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+    It contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+GuidevAudioProcessorEditor::GuidevAudioProcessorEditor (GuidevAudioProcessor& p)
+    : AudioProcessorEditor (&p), processor (p)
+{
+    // Make sure that before the constructor has finished, you've set the
+    // editor's size to whatever you need it to be.
+    setSize (200, 200);
+    
+    left.setButtonText("left");
+    right.setButtonText("right");
+
+    addAndMakeVisible(button);
+    addAndMakeVisible(left);
+    addAndMakeVisible(right);
+
+}
+
+GuidevAudioProcessorEditor::~GuidevAudioProcessorEditor()
+{
+}
+
+//==============================================================================
+void GuidevAudioProcessorEditor::paint (Graphics& g)
+{
+    // (Our component is opaque, so we must completely fill the background with a solid colour)
+    g.fillAll (Colours::black);
+
+    g.setColour (Colours::red);
+    g.setFont (15.0f);
+    Path leftTriangle;
+    leftTriangle.addTriangle(10, 10, getWidth() - 10, getWidth() - 10, getHeight(), getHeight());
+    g.fillPath (leftTriangle);
+    g.setColour (Colours::black);
+
+}
+
+void GuidevAudioProcessorEditor::resized()
+{
+    // This is generally where you'll want to lay out the positions of any
+    // subcomponents in your editor..
+    auto r = getLocalBounds();
+    button.setBounds(r.removeFromTop(150).reduced(10));
+    left.setBounds(r.removeFromLeft(100).reduced(10));
+    right.setBounds(r.removeFromLeft(100).reduced(10));
+
+}
---
+++ SimpleDelay/Source/PluginEditor.h
@@ -0,0 +1,39 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+    It contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include "../JuceLibraryCode/JuceHeader.h"
+#include "PluginProcessor.h"
+
+//==============================================================================
+/**
+*/
+class GuidevAudioProcessorEditor  : public AudioProcessorEditor
+{
+public:
+    GuidevAudioProcessorEditor (GuidevAudioProcessor&);
+    ~GuidevAudioProcessorEditor();
+
+    //==============================================================================
+    void paint (Graphics&) override;
+    void resized() override;
+
+private:
+    // This reference is provided as a quick way for your editor to
+    // access the processor object that created it.
+    GuidevAudioProcessor& processor;
+
+    TextButton button;
+    TextButton left;
+    TextButton right;
+    
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (GuidevAudioProcessorEditor)
+};
