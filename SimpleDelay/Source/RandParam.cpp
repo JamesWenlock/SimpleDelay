2332e595a829bbdfa59d62c11066887fb873e6cd
Mick Marchan <mick.marchan@gmail.com>
2019-01-17 13:54:01 -0800

    starting random parameter class

---
+++ SimpleDelay/Source/RandParam.cpp
@@ -0,0 +1,65 @@
+/*
+  ==============================================================================
+
+    RandParam.cpp
+    Created: 17 Jan 2019 1:07:37pm
+    Author:  Mick Marchan
+
+  ==============================================================================
+*/
+
+#include "RandParam.h"
+
+//==============================================================================
+RandParam::RandParam(int numChannels, bool hasSync, int *RNG, float lo,
+                     float hi, float syncLikelihood, float biLikelihood, std::string curve)
+{
+    this->numChannels = numChannels;
+    this->hasSync = hasSync;
+    this->lo = lo;
+    this->hi = hi;
+    this->syncLikelihood = syncLikelihood;
+    this->biLikelihood = biLikelihood;
+    this->curve = curve;
+    
+    // store RNG values
+    int rngSize = numChannels + hasSync;
+    this->RNG = (int *) malloc(sizeof(int) * rngSize);
+    for(int i = 0; i < rngSize; i++) {
+        this->RNG[i] = RNG[i];
+    }
+    
+    // create space for parameter values
+    this->val = (float *) malloc(sizeof(float) * numChannels);
+}
+
+RandParam::RandParam()
+{
+}
+
+RandParam::~RandParam()
+{
+    free(RNG);
+    free(val);
+}
+//==============================================================================
+void RandParam::updateParam(int seed) {
+    
+    bool isSync = false;
+    if (hasSync) {
+        isSync = getRNG(RNG[numChannels], seed) < syncLikelihood;
+    }
+    
+    for (int channel = 0; channel < numChannels; channel++) {
+        val[channel] = 
+    }
+}
+
+float RandParam::get(int channel) {
+    // error if channel out of range?
+    return val[channel];
+}
+
+float getRNG(int RNG, int seed) {
+    return (seed % RNG) / (float) RNG;
+}
---
+++ SimpleDelay/Source/RandParam.h
@@ -0,0 +1,39 @@
+/*
+  ==============================================================================
+
+    RandParam.h
+    Created: 17 Jan 2019 1:07:37pm
+    Author:  Mick Marchan
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <iostream>
+
+class RandParam
+{
+public:
+    RandParam();
+    RandParam(int numChannels, bool hasSync, int *RNG, float lo,
+              float hi, float syncLikelihood, float biLikelihood);
+    ~RandParam();
+    
+    //==============================================================================
+    void updateParam(int seed);
+    float get(int channel);
+    
+private:
+    int numChannels;
+    bool hasSync;
+    int *RNG;
+    float *val;
+    float lo;
+    float hi;
+    float syncLikelihood;
+    float biLikelihood;
+    std::string curve;
+    
+    float getRNG(int RNG, int seed);
+};
